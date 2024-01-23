02a71a87ddf60a7a699d8fff78971a38502301c2
Jamaტa <jamata94@gmail.com>
2024-01-24 03:11:23 +0400

    Update StandingViewController
    add UITableView
    add DriverTableCellView in UITableView

--- Racing Line/Scenes/Standing/StandingViewController.swift
+++ Racing Line/Scenes/Standing/StandingViewController.swift
@@ -13,12 +13,29 @@ final class StandingViewController: UIViewController, UITableViewDelegate {
     private var viewModel: StandingViewModel = StandingViewModel(networkManager: Network())
     private var driverStandings = [DriverStanding]()
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(AppColors.background)
         setupViewModelDelegate()
-        viewModel.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.pin(to: view)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.viewDidLoad()
     }
     
     private func setupViewModelDelegate() {
@@ -28,11 +45,33 @@ final class StandingViewController: UIViewController, UITableViewDelegate {
 
 extension StandingViewController: StandingViewModelDelegate {
     func standingInfoGot(_ data: StandingResponse) {
-        let drivers = data.mrData.standingsTable?.standingsLists.first?.driverStandings
-        driverStandings = drivers!
+        
+        guard let drivers = data.mrData.standingsTable?.standingsLists.first?.driverStandings else {
+            return
+        }
+        
+        driverStandings = drivers
+        tableView.reloadData()
     }
     
     func showError(_ error: Error) {
         print("error!")
+    }
+}
+
+extension StandingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        driverStandings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let driver = driverStandings[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.contentConfiguration = UIHostingConfiguration(content: {
+            DriverTableCellView(driverStanding: driver)
+        })
+        
+        return cell
     }
 }
