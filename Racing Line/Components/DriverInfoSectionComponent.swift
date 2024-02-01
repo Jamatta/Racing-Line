f6ecc6a2eff6aa59c94fffd4ddcd5c4e74d2f108
Jamaტa <jamata94@gmail.com>
2024-02-01 21:48:17 +0400

    Add DriverInfoSectionComponent

---
+++ Racing Line/Components/DriverInfoSectionComponent.swift
@@ -0,0 +1,42 @@
+//
+//  DriverInfoSectionComponent.swift
+//  Racing Line
+//
+//  Created by Nika Jamatashvili on 01.02.24.
+//
+
+import SwiftUI
+
+struct DriverInfoSectionComponent: View {
+    
+    //MARK: - Properties
+    var infoLabel: String
+    var infoText: String
+    
+    //MARK: - Body
+    var body: some View {
+        infoSectionView
+    }
+    
+    //MARK: - Components
+    private var infoSectionView: some View {
+        VStack(spacing: 8) {
+            Text(infoText)
+                .font(Font.system(size: 18))
+                .fontWeight(.black)
+                .foregroundStyle(AppColors.textPrimary)
+            
+            Text(infoLabel)
+                .font(Font.system(size: 14))
+                .foregroundStyle(AppColors.textSecondary)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 20)
+        .padding(.top, 8)
+        .padding(.vertical, 12)
+        .background(AppColors.background)
+        .cornerRadius(8)
+        
+    }
+}
